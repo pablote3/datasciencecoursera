2098ac0d52d2087bbc5d86f16f64161a8703c1c3
Paul Rossotti <rossotti.paul@gmail.com>
2018-01-22 20:18:29 -0800

    check if file exists

--- course5_reproducible/storm.R
+++ course5_reproducible/storm.R
@@ -1,6 +1,11 @@
-setwd("/home/pablote/Documents/r/coursera/5 Reproducible Research/assignment2/")
+setwd("~/Documents/r/coursera/5 Reproducible Research/assignment2/")
 #library(ggplot2)
 library(dplyr)
+
+if (!file.exists("repdata%2Fdata%2FStormData.csv.bz2")) {
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+    download.file(fileUrl, destfile = "repdata%2Fdata%2FStormData.csv.bz2", method = "curl")
+}
 
 storm <- read.csv("repdata%2Fdata%2FStormData.csv.bz2")
 #storm$BGN_DATE <- as.Date(storm$BGN_DATE)
